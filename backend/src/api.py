ddf0ed63bd80a2a357ca3ae3107c7bfa4aee7660
Geoffrey Willis <benwill2014@gmail.com>
2019-11-25 12:28:36 +0300

    bug(update npm packages) update high risk vulnerable npm packages
    - run `npm audit` and fix high risk packages with the recommended
      action

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
-import json
+from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
